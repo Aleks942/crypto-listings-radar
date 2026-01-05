0c7f3c2e9ce6113c9f0594cb4edad1b8af4388ba
Aleks942 <aleks.aw1978@gmail.com>
2026-01-05 23:50:38 +0100

    Create config_signals.py

---
+++ config_signals.py
@@ -0,0 +1,19 @@
+SIGNAL_CFG = {
+    "max_age_days": 14,
+    "ultra_age_days": 1,
+
+    "min_pairs_ultra": 1,
+    "min_volume_watch": 200_000,
+    "min_volume_ultra": 500_000,
+
+    "confirm_volume_ratio": 1.8,
+    "spike_volume_ratio": 2.5,
+
+    "confirm_price_chg_1h": 0.06,
+    "spike_price_chg_1h": 0.10,
+
+    "grade_A_score": 85,
+    "grade_B_score": 70,
+
+    "cooldown_minutes": 60,
+}
